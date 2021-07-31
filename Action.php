51b0f543fd7dd5bbf27d9cb3bb58035cd89c7ffd
CcDalao <842105949@qq.com>
2021-07-31 15:01:44 +0800

    first

---
+++ Action.php
@@ -0,0 +1,81 @@
+<?php
+function remove_xss($string) {
+    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string); $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base'); $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload'); $parm = array_merge($parm1, $parm2);
+
+    for ($i = 0; $i < sizeof($parm); $i++) {
+        $pattern = '/';
+        for ($j = 0; $j < strlen($parm[$i]); $j++) {
+            if ($j > 0) {
+                $pattern .= '(';
+                $pattern .= '(&#[x|X]0([9][a][b]);?)?';
+                $pattern .= '|(&#0([9][10][13]);?)?';
+                $pattern .= ')?';
+            } $pattern .= $parm[$i][$j];
+        } $pattern .= '/i'; $string = preg_replace($pattern, ' ', $string);
+    } return $string;
+}
+class ErcerLink_Action implements Widget_Interface_Do {
+    public function execute() {
+        //Do
+    }
+    public function action() {}
+    public function link_add() {
+
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+            die('欸！是不是走错路啦！😊');
+        } else {
+
+            function getUserIpAddr() {
+                if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+                } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                } else {
+                    $ip = $_SERVER['REMOTE_ADDR'];
+                }
+                return $ip;
+            }
+            $reallip = getUserIpAddr();
+            $host_name = remove_xss($_POST['host_name']);
+            $host_url = remove_xss($_POST['host_url']);
+            $host_png = remove_xss($_POST['host_png']);
+            $host_msg = remove_xss($_POST['host_msg']);
+            $host_yes = remove_xss($_POST['host_yes']);
+
+
+
+            $db = Typecho_Db::get();
+            $query_if_also_name = $db->select('name')->from('table.links')->where('name = ?', $host_name);
+            $result_name = $db->fetchAll($query_if_also_name);
+
+            $query_if_also_user = $db->select('user')->from('table.links')->where('user = ?', $reallip);
+            $result_user = $db->fetchAll($query_if_also_user);
+
+            if (!empty($result_name) or !empty($result_user)) {
+                die('欸！你已经提交过啦😊');
+            }
+
+
+
+
+            $insert = $db->insert('table.links')
+            ->rows(array(
+                'url' => $host_url,
+                'name' => $host_name,
+                'image' => $host_png,
+                'description' => $host_msg,
+                'user' => $reallip,
+                'sort' => 'others'
+            ));
+            $insertId = $db->query($insert);
+            
+
+            
+            
+            die('postok');
+        }
+
+    }
+
+
+}
---
+++ Plugin.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* <a href='https://blog.ccdalao.cn/archives/197/' target='_blank'><<< handsome友链快速申请插件 >>></a>
+*
+* @package <strong style="color:red;">ErcerLink</strong>
+* @author 二C
+* @version 1.0.3
+* @link https://blog.ccdalao.cn
+*/
+function higrid_compress_html($higrid_uncompress_html_source) {
+    $chunks = preg_split('/(<pre.*?\/pre>)/ms', $higrid_uncompress_html_source, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $higrid_uncompress_html_source = ''; //[higrid.net]修改压缩html : 清除换行符,清除制表符,去掉注释标记
+    foreach ($chunks as $c) {
+        if (strpos($c, '<pre') !== 0) {
+            $c = preg_replace('/[\\n\\r\\t]+/', ' ', $c);
+            $c = preg_replace('/\\s{2,}/', ' ', $c);
+            $c = preg_replace('/>\\s</', '><', $c);
+            $c = preg_replace('/\\/\\*.*?\\*\\//i', '', $c);
+        }
+        $higrid_uncompress_html_source .= $c;
+    }
+    return $higrid_uncompress_html_source;
+}
+class ErcerLink_Plugin implements Typecho_Plugin_Interface {
+
+
+    /* 插件配置方法 */
+    public static function config(Typecho_Widget_Helper_Form $form) {}
+
+    /* 个人用户的配置方法 */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+
+    /* 插件实现方法 */
+    public static function render() {}
+
+
+    public static function activate() {
+        Helper::addRoute("route_to_link_add", "/link_add", "ErcerLink_Action", 'link_add');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array('ErcerLink_Plugin', 'footer');
+    }
+
+    public static function deactivate() {
+        Helper::removeRoute("route_to_link_add");
+    }
+
+    public static function footer() {
+        $Path = Helper::options()->pluginUrl . '/ErcerLink/';
+        $ahh = higrid_compress_html(Content::returnLinkList("ten", "ErcerLink"));
+        echo '<script type="text/javascript" src="' . $Path . 'js/ErcerLinks.js?v=998"></script>';
+        echo ("
+        <script>
+            $(document).ready(function(){
+                $('#addhere').append('$ahh')
+            });
+        </script>
+        ");
+
+
+
+    }
+
+
+}
