fec6c93792d3eb59f32c77ec3c17edb53082b5ba
彭文凤_ercer <842105949@qq.com>
2025-03-09 22:43:55 +0800

    Update Action.php

--- Action.php
+++ Action.php
@@ -1,6 +1,9 @@
 <?php
 function remove_xss($string) {
-    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string); $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base'); $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload'); $parm = array_merge($parm1, $parm2);
+    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string); 
+    $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base'); 
+    $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload'); 
+    $parm = array_merge($parm1, $parm2);
 
     for ($i = 0; $i < sizeof($parm); $i++) {
         $pattern = '/';
@@ -10,21 +13,24 @@ function remove_xss($string) {
                 $pattern .= '(&#[x|X]0([9][a][b]);?)?';
                 $pattern .= '|(&#0([9][10][13]);?)?';
                 $pattern .= ')?';
-            } $pattern .= $parm[$i][$j];
-        } $pattern .= '/i'; $string = preg_replace($pattern, ' ', $string);
-    } return $string;
+            } 
+            $pattern .= $parm[$i][$j];
+        } 
+        $pattern .= '/i'; 
+        $string = preg_replace($pattern, ' ', $string);
+    } 
+    return $string;
 }
+
 class ErcerLink_Action extends Typecho_Widget implements Widget_Interface_Do {
     public function execute() {
         //Do
     }
     public function action() {}
     public function link_add() {
-
         if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             die('欸！是不是走错路啦！😊');
         } else {
-
             function getUserIpAddr() {
                 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
                     $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -36,20 +42,22 @@ class ErcerLink_Action extends Typecho_Widget implements Widget_Interface_Do {
                 return $ip;
             }
             $reallip = getUserIpAddr();
-            $host_name = remove_xss($_POST['host_name']);
-            $host_url = remove_xss($_POST['host_url']);
-            $host_png = remove_xss($_POST['host_png']);
-            $host_msg = remove_xss($_POST['host_msg']);
-            $host_yes = remove_xss($_POST['host_yes']);
-            if(empty($_POST['host_name'])){
-            $host_name = remove_xss($_POST['host_name_page']);
-            $host_url = remove_xss($_POST['host_url_page']);
-            $host_png = remove_xss($_POST['host_png_page']);
-            $host_msg = remove_xss($_POST['host_msg_page']);
-            $host_yes = remove_xss($_POST['host_yes_page']);
+
+            // 获取表单数据，并为 host_yes 设置默认值
+            $host_name = remove_xss($_POST['host_name'] ?? '');
+            $host_url = remove_xss($_POST['host_url'] ?? '');
+            $host_png = remove_xss($_POST['host_png'] ?? '');
+            $host_msg = remove_xss($_POST['host_msg'] ?? '');
+            $host_yes = remove_xss($_POST['host_yes'] ?? 'no'); // 默认值为 'no'
+
+            // 如果是从页面表单提交的，获取对应的值
+            if (empty($host_name)) {
+                $host_name = remove_xss($_POST['host_name_page'] ?? '');
+                $host_url = remove_xss($_POST['host_url_page'] ?? '');
+                $host_png = remove_xss($_POST['host_png_page'] ?? '');
+                $host_msg = remove_xss($_POST['host_msg_page'] ?? '');
+                $host_yes = remove_xss($_POST['host_yes_page'] ?? 'no'); // 默认值为 'no'
             }
-
-
 
             $db = Typecho_Db::get();
             $query_if_also_name = $db->select('name')->from('table.links')->where('name = ?', $host_name);
@@ -63,22 +71,16 @@ class ErcerLink_Action extends Typecho_Widget implements Widget_Interface_Do {
             }
 
             $insert = $db->insert('table.links')
-            ->rows(array(
-                'url' => $host_url,
-                'name' => $host_name,
-                'image' => $host_png,
-                'description' => $host_msg,
-                'user' => $reallip,
-                'sort' => 'others'
-            ));
+                ->rows(array(
+                    'url' => $host_url,
+                    'name' => $host_name,
+                    'image' => $host_png,
+                    'description' => $host_msg,
+                    'user' => $reallip,
+                    'sort' => 'others'
+                ));
             $insertId = $db->query($insert);
             die('postok');
         }
-
     }
-
-
-
-
-
 }
